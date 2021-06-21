3c3133e0f91c365073024a00d314f24d627ae35e
Bradley van der Westhuizen <bradleyvanderwesthuizen7@gmail.com>
2021-06-21 14:21:08 +0200

    first commit

---
+++ BethanysPieShop/Migrations/20210610150637_NotesAddedOnPie.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class NotesAddedOnPie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Pies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 1,
+                column: "IsPiesOfTheWeek",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 8,
+                column: "IsPiesOfTheWeek",
+                value: true);
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 9,
+                column: "IsPiesOfTheWeek",
+                value: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Pies");
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 1,
+                column: "IsPiesOfTheWeek",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 8,
+                column: "IsPiesOfTheWeek",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Pies",
+                keyColumn: "PieId",
+                keyValue: 9,
+                column: "IsPiesOfTheWeek",
+                value: false);
+        }
+    }
+}
